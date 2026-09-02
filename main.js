2e35ac1e21d41594d03b8a6dfd8c8f8e96acdb96
Nosebleed <gachaplay228@gmail.com>
2026-09-02 18:10:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,10 @@ client.once('clientReady', () => [
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
 
-  if (message.content.includes("https://cdn.discordapp.com/attachments/1519657113175986348/1522643260630237434/togif.gif" || "https://cdn.discordapp.com/attachments/1458159628373463192/1544722080094883960/cool_bear.gif")){
+  if (
+    message.content.includes("https://cdn.discordapp.com/attachments/1519657113175986348/1522643260630237434/togif.gif") || 
+    message.content.includes("https://cdn.discordapp.com/attachments/1458159628373463192/1544722080094883960/cool_bear.gif")){
+    
     console.log('Deleted message with medved Vlad');
     message.delete();
   } else {
